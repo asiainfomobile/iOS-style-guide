600c78ad0ecae0a736d60f82f6199c9dc74e9657
zhengxin <zx19880528@gmail.com>
2016-03-01 15:33:48 +0800

    modified:   demo/demo/demo/ViewControllers/TimeLineView.swift

--- demo/demo/demo/ViewControllers/TimeLineView.swift
+++ demo/demo/demo/ViewControllers/TimeLineView.swift
@@ -13,12 +13,15 @@ class TimeLineView: UIView {
 	@IBOutlet weak var logo2: UIImageView!
 	@IBOutlet weak var logo3: UIImageView!
 	
+	@IBOutlet weak var topCenterXConstraint: NSLayoutConstraint!
+	@IBOutlet weak var middleCenterXConstraint: NSLayoutConstraint!
+	@IBOutlet weak var bottomCenterXConstraint: NSLayoutConstraint!
+	
 	var line1: CAShapeLayer!
 	var line2: CAShapeLayer!
 	
 	var path1: CGPath {
 		let point1 = CGPoint(x: CGRectGetMidX(logo1.frame), y: CGRectGetMaxY(logo1.frame))
-		
 		let point2 = CGPoint(x: CGRectGetMidX(logo2.frame), y: CGRectGetMinY(logo2.frame))
 		
 		let path1 = UIBezierPath()
@@ -29,14 +32,13 @@ class TimeLineView: UIView {
 	}
 	
 	var path2: CGPath {
-		
 		let point3 = CGPoint(x: CGRectGetMidX(logo2.frame), y: CGRectGetMaxY(logo2.frame))
-		
 		let point4 = CGPoint(x: CGRectGetMidX(logo3.frame), y: CGRectGetMinY(logo3.frame))
 		
 		let path2 = UIBezierPath()
 		path2.moveToPoint(point3)
 		path2.addLineToPoint(point4)
+        
 		return path2.CGPath
 	}
 	
@@ -57,9 +59,7 @@ class TimeLineView: UIView {
 	}
 	
 	func animationLines() {
-		
 		let animation1 = makeAnimationToNewPath(path1)
-		
 		let animation2 = makeAnimationToNewPath(path2)
 		
 		line1.addAnimation(animation1, forKey: nil)
@@ -67,24 +67,8 @@ class TimeLineView: UIView {
 	}
 	
 	override func displayLayer(layer: CALayer) {
-		let point1 = CGPoint(x: CGRectGetMidX(logo1.frame), y: CGRectGetMaxY(logo1.frame))
-		
-		let point2 = CGPoint(x: CGRectGetMidX(logo2.frame), y: CGRectGetMinY(logo2.frame))
-		
-		let point3 = CGPoint(x: CGRectGetMidX(logo2.frame), y: CGRectGetMaxY(logo2.frame))
-		
-		let point4 = CGPoint(x: CGRectGetMidX(logo3.frame), y: CGRectGetMinY(logo3.frame))
-		
-		let path1 = UIBezierPath()
-		path1.moveToPoint(point1)
-		path1.addLineToPoint(point2)
-		
-		let path2 = UIBezierPath()
-		path2.moveToPoint(point3)
-		path2.addLineToPoint(point4)
-		
-		line1.path = path1.CGPath
-		line2.path = path2.CGPath
+		line1.path = path1
+		line2.path = path2
 	}
 	
 	func makeAnimationToNewPath(newPath: CGPath) -> CABasicAnimation {
@@ -96,10 +80,6 @@ class TimeLineView: UIView {
 		animation.toValue = newPath
 		return animation
 	}
-	
-	@IBOutlet weak var topCenterXConstraint: NSLayoutConstraint!
-	@IBOutlet weak var middleCenterXConstraint: NSLayoutConstraint!
-	@IBOutlet weak var bottomCenterXConstraint: NSLayoutConstraint!
 	
 	@IBAction func topTapped(sender: AnyObject) {
 		UIView.animateWithDuration(0.25) { () -> Void in
